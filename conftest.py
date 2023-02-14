08aada89dfc78a0689ae51139b32592c8fc7a68e
anna-ste <anna.ste0502@gmail.com>
2023-02-14 01:29:48 +0100

    Add playwright browser configuration

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture
+def page():
+    with sync_playwright() as playwright:
+        chromium = playwright.chromium
+        browser = chromium.launch(headless=False)
+        context = browser.new_context()
+
+        # add consent cookies
+        context.add_cookies([{'name': 'CookieConsent',
+                              'value': '{stamp:%27gbo7eV4cypV28EbMhnSTvVoyZ6PUIsnQwehj5C3XLqRxn0JeUv27Dw==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1676332448497000%2Cregion:%27nl%27}',
+                              'url': 'https://swapfiets.co.uk'}])
+        context.add_cookies([{'name': 'CookieConsent',
+                              'value': '{stamp:%27gbo7eV4cypV28EbMhnSTvVoyZ6PUIsnQwehj5C3XLqRxn0JeUv27Dw==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1676332448497000%2Cregion:%27nl%27}',
+                              'url': 'https://account.swapfiets.com'}])
+
+        # create a new page inside context.
+        page = context.new_page()
+        yield page
+        context.close()
