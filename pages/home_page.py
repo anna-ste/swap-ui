5413beedbb9ec41d05ba5eeaaab5dfe1d798a3cf
anna-ste <anna.ste0502@gmail.com>
2023-02-14 01:56:15 +0100

    Add title to home page class

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,9 +2,11 @@ from pages.city_page import CityPage
 
 
 class HomePage:
+    TITLE = 'Swapfiets - Your bike for a fixed monthly fee.'
 
     def __init__(self, page):
         self.page = page
+        self.page.wait_for_load_state("networkidle")
         self.city_input = page.locator('//input[@data-test-id="city-selector-input"]')
         self.see_bikes_button = page.locator("//button[@data-test-id='city-selector-submit']")
 
